095299feb4e33f8afac6697b927772bf87ab63d2
Róbert Selvek <robert.selvek@ataccama.com>
2020-02-04 14:02:21 +0100

    Run formatter

--- appindicator/src/main.rs
+++ appindicator/src/main.rs
@@ -1,11 +1,11 @@
 mod session;
 
+use dbus::blocking::Connection;
+use glib::source::{timeout_add_local, Continue};
 use gtk::prelude::*;
 use libappindicator::{AppIndicator, AppIndicatorStatus_APP_INDICATOR_STATUS_ACTIVE};
-use glib::source::{Continue, timeout_add_local};
-use dbus::blocking::{Connection};
-use std::time::Duration;
 use session::Manager;
+use std::time::Duration;
 
 fn main() {
     gtk::init().unwrap();
@@ -16,7 +16,11 @@ fn main() {
     // indicator.set_icon_full(path.to_str().unwrap(), "icon");
     timeout_add_local(1000, move || {
         let conn = Connection::new_system().unwrap();
-        let proxy = conn.with_proxy("org.freedesktop.login1", "/org/freedesktop/login1", Duration::new(5, 0));
+        let proxy = conn.with_proxy(
+            "org.freedesktop.login1",
+            "/org/freedesktop/login1",
+            Duration::new(5, 0),
+        );
         let inhibitors = proxy.list_inhibitors().unwrap();
         let mut m = gtk::Menu::new();
         for (_, app, _, _, _, _) in inhibitors {
@@ -26,7 +30,7 @@ fn main() {
                 .build();
             m.append(&mi);
         }
-        indicator.set_menu(&mut m); 
+        indicator.set_menu(&mut m);
         m.show_all();
         Continue(true)
     });
