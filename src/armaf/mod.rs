818b482ed64dab73f3456d127b4a43feb4ef3385
Róbert Selvek <selverob@fit.cvut.cz>
2021-10-04 12:30:09 +0200

    Add test controller and sensor

--- src/armaf/mod.rs
+++ src/armaf/mod.rs
@@ -1,75 +1,9 @@
 pub mod test_sensor;
 pub mod test_controller;
+pub mod actors;
+pub mod effector;
 
-use core::fmt;
-// use std::sync::Arc;
-// use std::{future::Future};
-use std::error::Error;
-use std::result::Result;
-
-use tokio::sync::mpsc::error::SendError;
-use tokio::sync::{mpsc, oneshot};
-
-type ResponseReceiver<R, E> = oneshot::Receiver<Result<R, E>>;
-
-pub struct Request<P, R, E> {
-    payload: P,
-    response_sender: oneshot::Sender<Result<R, E>>,
-}
-
-impl<P, R, E> Request<P, R, E> {
-    pub fn new(payload: P) -> (Request<P, R, E>, ResponseReceiver<R, E>) {
-        let (response_sender, response_receiver) = oneshot::channel();
-        let request = Request {
-            payload,
-            response_sender,
-        };
-        (request, response_receiver)
-    }
-
-    pub fn respond(self, response: Result<R, E>) -> Result<(), Result<R, E>> {
-        self.response_sender.send(response)
-    }
-}
-
-#[derive(Debug)]
-pub enum ActorRequestError<E> {
-    SendError,
-    RecvError,
-    ActorError(E),
-}
-
-#[derive(Clone, Debug)]
-pub struct ActorPort<P, R, E> {
-    message_sender: mpsc::Sender<Request<P, R, E>>,
-}
-
-impl<P, R, E> ActorPort<P, R, E> {
-    pub fn new(message_sender: mpsc::Sender<Request<P, R, E>>) -> ActorPort<P, R, E> {
-        ActorPort{message_sender}
-    }
-
-    pub async fn raw_request(
-        &self,
-        r: Request<P, R, E>,
-    ) -> Result<(), SendError<Request<P, R, E>>> {
-        self.message_sender.send(r).await
-    }
-
-    pub async fn request(&self, payload: P) -> Result<R, ActorRequestError<E>> {
-        let (req, rx) = Request::new(payload);
-        if self.raw_request(req).await.is_err() {
-            return Err(ActorRequestError::SendError);
-        }
-        match rx.await {
-            Err(_) => Err(ActorRequestError::RecvError),
-            Ok(inner_result) => match inner_result {
-                Ok(response) => Ok(response),
-                Err(actor_error) => Err(ActorRequestError::ActorError(actor_error)),
-            },
-        }
-    }
-}
+pub use actors::*;
 
 // pub enum ActorRequest {
 //     Apply,
