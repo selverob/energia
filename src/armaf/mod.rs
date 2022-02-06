cd1c71000be1bddf24bcfe4f86719efa3ae1f287
Róbert Selvek <robert@selvek.me>
2022-02-06 13:55:35 +0100

    Add documentation for armaf

--- src/armaf/mod.rs
+++ src/armaf/mod.rs
@@ -1,7 +1,14 @@
-pub mod test_sensor;
-pub mod test_controller;
-pub mod actors;
-pub mod effector;
+//! A framework for working with actor-based software systems loosely based on
+//! the "Actor-based Runtime Model of Adaptable Feedback Control Loops" paper. 
 
+mod actors;
+mod effector;
+
+pub mod test_controller;
+pub mod test_sensor;
+
+#[doc(inline)]
 pub use actors::*;
+
+//#[doc(inline)]
 pub use effector::{EffectorMessage, EffectorPort};
