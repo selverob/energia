e66f436522eb9810da7463349904abb6c86c66b0
Róbert Selvek <robert@selvek.me>
2022-02-13 19:34:19 +0100

    chore: fix formatting

--- src/system/inhibition_sensor.rs
+++ src/system/inhibition_sensor.rs
@@ -1,12 +1,15 @@
 use crate::armaf::{self, ActorPort};
-use logind_zbus::manager;
 use log;
+use logind_zbus::manager;
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub struct GetInhibitions;
 
-pub fn spawn(connection: zbus::Connection) -> ActorPort<GetInhibitions, Vec<manager::Inhibitor>, anyhow::Error> {
-    let (port, mut rx) = ActorPort::<GetInhibitions, Vec<manager::Inhibitor>, anyhow::Error>::make();
+pub fn spawn(
+    connection: zbus::Connection,
+) -> ActorPort<GetInhibitions, Vec<manager::Inhibitor>, anyhow::Error> {
+    let (port, mut rx) =
+        ActorPort::<GetInhibitions, Vec<manager::Inhibitor>, anyhow::Error>::make();
     tokio::spawn(async move {
         log::debug!("Initializing manager proxy");
         match logind_zbus::manager::ManagerProxy::new(&connection).await {
@@ -16,26 +19,33 @@ pub fn spawn(connection: zbus::Connection) -> ActorPort<GetInhibitions, Vec<mana
                     Some(req) => {
                         log::info!("Got message");
                         let logind_response = proxy.list_inhibitors().await;
-                        req.respond(as_anyhow_result(logind_response)).expect("request response failed, is idleness controller dead?");
+                        req.respond(as_anyhow_result(logind_response))
+                            .expect("request response failed, is idleness controller dead?");
                     }
                     None => {
                         log::info!("Stopping");
                         return;
                     }
                 }
-            },
+            }
             Err(error) => {
                 log::error!("Couldn't create a logind manager proxy: {}", error);
-                armaf::error_loop(rx, "inhibition sensor failed to create login manager proxy".to_owned()).await;
+                armaf::error_loop(
+                    rx,
+                    "inhibition sensor failed to create login manager proxy".to_owned(),
+                )
+                .await;
             }
         }
     });
     port
 }
 
-fn as_anyhow_result<T, E: std::error::Error + Send + Sync + 'static>(result: Result<T, E>) -> anyhow::Result<T> {
+fn as_anyhow_result<T, E: std::error::Error + Send + Sync + 'static>(
+    result: Result<T, E>,
+) -> anyhow::Result<T> {
     match result {
         Ok(t) => Ok(t),
-        Err(e) => Err(anyhow::Error::new(e))
+        Err(e) => Err(anyhow::Error::new(e)),
     }
 }
--- src/system/test/inhibition_sensor_test.rs
+++ src/system/test/inhibition_sensor_test.rs
@@ -1,9 +1,9 @@
+use crate::external::dbus::ConnectionFactory;
+use crate::system::inhibition_sensor;
+use logind_zbus::manager;
 use std::os::unix::prelude::FromRawFd;
 use tokio;
-use logind_zbus::{manager};
 use zvariant::OwnedFd;
-use crate::system::inhibition_sensor;
-use crate::external::dbus::ConnectionFactory;
 
 #[tokio::test]
 async fn test_inhibition_sensor() {
@@ -11,10 +11,27 @@ async fn test_inhibition_sensor() {
     let test_connection = factory.get_system().await.unwrap();
     let manager_proxy = manager::ManagerProxy::new(&test_connection).await.unwrap();
     let port = inhibition_sensor::spawn(factory.get_system().await.unwrap());
-    let inhibition_fd = manager_proxy.inhibit(manager::InhibitType::Idle, "energia tests", "testing idleness manager", "block").await.unwrap();
-    let inhibitors = port.request(inhibition_sensor::GetInhibitions).await.expect("inhibition sensor internal error");
-    let our_inhibitor = inhibitors.iter().find(|i| i.who() == "energia tests").unwrap();
-    assert_eq!(our_inhibitor.what().types(), &vec![manager::InhibitType::Idle]);
+    let inhibition_fd = manager_proxy
+        .inhibit(
+            manager::InhibitType::Idle,
+            "energia tests",
+            "testing idleness manager",
+            "block",
+        )
+        .await
+        .unwrap();
+    let inhibitors = port
+        .request(inhibition_sensor::GetInhibitions)
+        .await
+        .expect("inhibition sensor internal error");
+    let our_inhibitor = inhibitors
+        .iter()
+        .find(|i| i.who() == "energia tests")
+        .unwrap();
+    assert_eq!(
+        our_inhibitor.what().types(),
+        &vec![manager::InhibitType::Idle]
+    );
     assert_eq!(our_inhibitor.why(), "testing idleness manager");
     assert_eq!(our_inhibitor.mode(), manager::Mode::Block);
     drop(inhibition_fd);
