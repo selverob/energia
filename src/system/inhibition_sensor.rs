a502d02d5cfedf8688a674f7521c2d2588d80468
Róbert Selvek <robert@selvek.me>
2022-02-18 20:14:47 +0100

    inhibition_sensor: fix sensor terminating after the first request

--- src/system/inhibition_sensor.rs
+++ src/system/inhibition_sensor.rs
@@ -1,32 +1,20 @@
-use crate::armaf::{self, ActorPort};
+use crate::armaf::{self, ActorPort, Request};
 use log;
-use logind_zbus::manager;
-
+use logind_zbus::manager::{self, ManagerProxy};
+use tokio::sync::mpsc::Receiver;
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub struct GetInhibitions;
 
 pub fn spawn(
     connection: zbus::Connection,
 ) -> ActorPort<GetInhibitions, Vec<manager::Inhibitor>, anyhow::Error> {
-    let (port, mut rx) =
+    let (port, rx) =
         ActorPort::<GetInhibitions, Vec<manager::Inhibitor>, anyhow::Error>::make();
     tokio::spawn(async move {
         log::debug!("Initializing manager proxy");
         match logind_zbus::manager::ManagerProxy::new(&connection).await {
             Ok(proxy) => {
-                log::info!("Started");
-                match rx.recv().await {
-                    Some(req) => {
-                        log::info!("Got message");
-                        let logind_response = proxy.list_inhibitors().await;
-                        req.respond(as_anyhow_result(logind_response))
-                            .expect("request response failed, is idleness controller dead?");
-                    }
-                    None => {
-                        log::info!("Stopping");
-                        return;
-                    }
-                }
+                processing_loop(rx, proxy).await;
             }
             Err(error) => {
                 log::error!("Couldn't create a logind manager proxy: {}", error);
@@ -39,6 +27,24 @@ pub fn spawn(
         }
     });
     port
+}
+
+async fn processing_loop(mut rx: Receiver<Request<GetInhibitions, Vec<manager::Inhibitor>, anyhow::Error>>, proxy: ManagerProxy<'_>) {
+    log::info!("Started");
+    loop {
+        match rx.recv().await {
+            Some(req) => {
+                log::info!("Got message");
+                let logind_response = proxy.list_inhibitors().await;
+                req.respond(as_anyhow_result(logind_response))
+                    .expect("request response failed, is idleness controller dead?");
+            }
+            None => {
+                log::info!("Stopping");
+                return;
+            }
+        }
+    }
 }
 
 fn as_anyhow_result<T, E: std::error::Error + Send + Sync + 'static>(
